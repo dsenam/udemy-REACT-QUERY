c3f5597b62a9190ca08491ce88cb29e33d1e6445
“Douglas <dsena.m@hotmail.com>
2023-02-18 18:12:38 -0300

    feat: configure react query and make a request

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
@@ -15,7 +15,11 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const {data, isError, isLoading} = useQuery("posts", fetchPosts);
+
+  if(isError) return <h3>Oops, somenthing went wrong</h3>
+
+  if(isLoading) return <h3>Carregando ....</h3>
 
   return (
     <>
